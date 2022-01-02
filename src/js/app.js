e207e7e93882925905a3e1baf9c4aad150985464
Martin <mdsugarev@gmail.com>
2022-01-02 19:07:44 +0200

    one

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,21 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
+
   const ul = document.querySelector("ul");
+  const myPokemons = async () => {
+    await fetch('https://pokeapi.co/api/v2/pokemon')
+    .then(res => res.json())
+    .then(data => {
+     for(let i = 0; i < 10; i++){
+       let p = data.results[i].name;
+       let el = document.createElement("li");
+       el.innerText = p;
+       ul.appendChild(el);
+     }
+    })
+    .catch(err => console.log(err))
+  }
+  myPokemons();
+
 });
